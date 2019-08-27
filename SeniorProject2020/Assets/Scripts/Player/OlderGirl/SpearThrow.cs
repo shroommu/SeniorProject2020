9e3602886b5e3eaae0876fa77afa91f4b4d9d06b
Alexa <mouldy.k@gmail.com>
2019-08-27 15:47:07 -0600

    added older girl spear throwing

---
+++ SeniorProject2020/Assets/Scripts/Player/OlderGirl/SpearThrow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearThrow : MonoBehaviour
+{
+    public GameObject spear;
+    public float spearSpeed = 1;
+    public bool canThrow = true;
+
+    public void Throw()
+    {
+        if(canThrow)
+        {
+            spear.transform.parent = null;
+            
+            Rigidbody spearRB = spear.GetComponent<Rigidbody>();
+            spearRB.useGravity = true;
+            spearRB.isKinematic = false;
+            spearRB.AddForce(spear.transform.forward * spearSpeed);
+            canThrow = false;
+            print("Throw");
+        }
+    }
+
+    public void Return()
+    {
+        print("Return");
+    }
+}
