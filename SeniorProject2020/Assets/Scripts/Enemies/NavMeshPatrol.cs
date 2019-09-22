df796f376f83bf9ff9de6ab6364f91dcb03e6da3
Alexa <mouldy.k@gmail.com>
2019-09-21 22:29:23 -0600

    added beginnings of enemy ai

---
+++ SeniorProject2020/Assets/Scripts/Enemies/NavMeshPatrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavMeshPatrol : MonoBehaviour
+{
+    public bool canPatrol = false;
+
+    public void StartPatrol()
+    {
+        canPatrol = true;
+        StartCoroutine(Patrol());
+    }
+
+    public void StopPatrol()
+    {
+        canPatrol = false;
+    }
+
+    IEnumerator Patrol()
+    {
+        while(canPatrol)
+        {
+            print("Patrolling");
+            yield return null;
+        }
+    }
+
+}
