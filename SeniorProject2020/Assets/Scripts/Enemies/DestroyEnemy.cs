6ed2df04583f86b8a41eca47c45053dc9db8a25d
Alexa <mouldy.k@gmail.com>
2019-10-06 16:05:23 -0600

    minor file organization, got enemy death state working

---
+++ SeniorProject2020/Assets/Scripts/Enemies/DestroyEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour
+{
+
+    public GameObject destroyPS;
+
+    public void Destroy()
+    {
+        print("Destroying enemy");
+        Instantiate(destroyPS, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
