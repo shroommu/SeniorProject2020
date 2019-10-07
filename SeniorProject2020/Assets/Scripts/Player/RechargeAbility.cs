129e1a2f4c1f7236a63737cfab9e0fb5d9009044
Alexa <mouldy.k@gmail.com>
2019-10-07 11:21:44 -0600

    added hdrp

---
+++ SeniorProject2020/Assets/Scripts/Player/RechargeAbility.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RechargeAbility : MonoBehaviour
+{
+    public float rechargeTime;
+    public UnityEvent notChargedEvent;
+    public UnityEvent chargedEvent;
+    public bool isRecharging;
+
+    public void StartRecharge()
+    {
+        isRecharging = true;
+        StartCoroutine(Recharge())
+    }
+
+    public void StopRecharge()
+    {
+        isRecharging = false;
+    }
+
+    IEnumerator Recharge()
+    {
+        while(isRecharging)
+        {
+            yield return new WaitForSeconds(rechargeTime);
+            isRecharging = false;
+        }
+    }
+
+    public void UseAbility()
+    {
+        if(isRecharging)
+        {
+            notChargedEvent.Invoke();
+        }
+        else
+        {
+            chargedEvent.Invoke();
+        }
+    }
+}
